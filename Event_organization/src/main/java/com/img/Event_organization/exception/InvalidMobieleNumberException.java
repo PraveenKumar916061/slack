d52587d07a1ad1222dc4b6a3b82fb72c4132a511
PraveenKumar916061 <128087559+PraveenKumar916061@users.noreply.github.com>
2023-08-30 12:46:39 +0530

    Delete Event_organization directory

--- Event_organization/src/main/java/com/img/Event_organization/exception/InvalidMobieleNumberException.java
+++
@@ -1,7 +0,0 @@
-package com.img.Event_organization.exception;
-
-public class InvalidMobieleNumberException extends Exception{
-    public InvalidMobieleNumberException(){
-        System.out.println("Invalid mobile Number Pls..! enter correct mobile number......." );
-    }
-}
--- Event_organization/src/main/java/com/img/Event_organization/repository/TeamRepository.java
+++
@@ -1,7 +0,0 @@
-package com.img.Event_organization.repository;
-
-import com.img.Event_organization.entity.Team;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TeamRepository extends JpaRepository<Team,Integer> {
-}
